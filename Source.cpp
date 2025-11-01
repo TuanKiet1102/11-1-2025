d148415b5858a59a3fe1c13a2f97cd09f25b8d84
Kiet <le2pham0tuan0kiet4@gmai.com>
2025-11-01 08:36:27 +0700

    Bai 1 va 2"

--- Source.cpp
+++ Source.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
 
 using namespace std;
-int tong(int a, int b);
-int hieu(int a, int b);
+int tong(int a, int b) {
+	return a + b;
+}
+int hieu(int a, int b) {
+	return a - b;
+}
 int tich(int a, int b);
 float thuong(int a, int b);
 int main()
 {
+	int a, b;
+	cout << "Nhap so thu nhat: ";
+	cin >> a;
+	cout << "Nhap so thu hai: ";
+	cin >> b;
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-	cout<<"Tong =";
+	cout << "Tong = " << tong(a, b) << endl;
+	cout << "Hieu = " << hieu(a, b) << endl;
 	system("pause");
 	return 0;
 }
